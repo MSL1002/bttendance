f53971b5ddf328dd2ac984519a2726195bf0e78e
Izzy J. <8mrl1002@gmail.com>
2026-05-26 16:06:08 -0600

    updated micro pico logic to take advantage of onboard light

--- bttendance_pico/main.py
+++ bttendance_pico/main.py
@@ -27,11 +27,22 @@ import gc
 import json
 
 reader = MFRC522(spi_id=0,sck=6,miso=4,mosi=7,cs=5,rst=22)
+LED = Pin("LED", Pin.OUT)
+
+def disconnect_service():
+    wlan.disconnect()
+    response.close()
+
+def flash_LED(num_flashes):
+    for i in range(num_flashes):
+        LED.toggle()
+        time.sleep(.5)
+        LED.toggle()
 
 
 with open("env.json") as f:
     env = json.load(f)
-
+    
 # WiFi credentials
 SSID = env.get("SSID")
 PASSWORD = env.get("PASSWORD")
@@ -39,48 +50,56 @@ PASSWORD = env.get("PASSWORD")
 # Connect to WiFi
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect(SSID, PASSWORD)
 
-while not wlan.isconnected():
-    print('Attempting to Connect to WiFi...')
-    Pin("LED", Pin.OUT).value(1)
-    time.sleep(1)
+try:
+    
+    wlan.connect(SSID, PASSWORD)
 
-print('Connected! IP:', wlan.ifconfig()[0])
-Pin("LED", Pin.OUT).value(0)
+    while not wlan.isconnected():
+        print('Attempting to Connect to WiFi...')
+        LED.toggle()
+        time.sleep(1)
 
-print("Awaiting ID")
-print("")
+    print('Connected! IP:', wlan.ifconfig()[0])
+    LED.toggle()
 
-while True:
-    reader.init()
-    (stat, tag_type) = reader.request(reader.REQIDL)
-    if stat == reader.OK:
-        (stat, uid) = reader.SelectTagSN()
+    print("Awaiting ID")
+    print("")
+
+    while True:
+        reader.init()
+        (stat, tag_type) = reader.request(reader.REQIDL)
         if stat == reader.OK:
-            rfid = int.from_bytes(bytes(uid),"little",False)
+            (stat, uid) = reader.SelectTagSN()
+            if stat == reader.OK:
+                rfid = int.from_bytes(bytes(uid),"little",False)
 
-            SERVER_IP = env.get("SERVER_IP")
-            SERVER_PORT = env.get("SERVER_PORT")
-            url = f"http://{SERVER_IP}:{SERVER_PORT}/log-scan?rfid={rfid}&location={env.get("LOCATION")}"
+                SERVER_IP = env.get("SERVER_IP")
+                SERVER_PORT = env.get("SERVER_PORT")
+                url = f"http://{SERVER_IP}:{SERVER_PORT}/log-scan?rfid={rfid}&location={env.get("LOCATION")}"
 
-            def try_connection():
-                print("sending request")
-                response = urequests.post(url)
-                print("Response:", response.text,"\n")
+                def try_connection():
+                    print("sending request")
+                    response = urequests.post(url)
+                    print("Response:", response.text,"\n")
 
-            try:
-                try_connection()
-                Pin("LED", Pin.OUT).value(1)
+                try:
+                    try_connection()
+                    flash_LED(2)
+                except Exception as e:
+                    print("Error sending request:", e)
+                    flash_LED(5)
+
+                response.close()
                 time.sleep(1)
-                Pin("LED", Pin.OUT).value(0)
-            except Exception as e:
-                print("Error sending request:", e)
+                
+                print("Awaiting ID")
+                print("")
 
-            time.sleep(1)
-            
-            print("Awaiting ID")
-            print("")
-
-            
-    gc.collect()
+                
+        gc.collect()
+finally:
+    print("performing clean up...")
+    wlan.disconnect()
+    LED.off()
+    print("clean up OK.")
