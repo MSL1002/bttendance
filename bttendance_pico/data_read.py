fd860b4ef4798d0fb12885de5b4017335a0bd754
Izzy J. <8mrl1002@gmail.com>
2026-05-26 18:02:19 -0600

    started work on adding students through RFID reader

--- bttendance_pico/data_read.py
+++ bttendance_pico/data_read.py
@@ -1,3 +1,25 @@
+'''
+
+This needs to get an RFID + collaberate with the front end to input a student into the DB
+
+take in RFID from here
+take in first name, last name, and student ID from website
+
+potential solution:
+
+Admin inputs student info on website
+website waits until it gets a POST from the pico
+
+Specialized Pico (with this file as it's main.py) posts to special endpoint to add the RFID to the student
+
+Once we get the RFID, we wrap the student info up
+enter it into the DB
+
+return website to home page
+make scanner await another scan
+
+'''
+
 from mfrc522 import MFRC522
 import time
 
